3331bf2204222704c65b49233a96d1227a8da310
dotmao <dorothy.hongqian@gmail.com>
2018-03-15 22:00:56 -0400

    updated js 03/15

--- app.js
+++ app.js
@@ -1,47 +1,4 @@
 function onReady() {
-// const addToDoForm = document.getElementById('addToDoForm')
-//   const newToDoText = document.getElementById('newToDoText')
-//   const toDoList = document.getElementById('toDoList')
-//   //const deleteButton = document.getElementById('deleteButton') //access the element deleteButton
-//
-//   addToDoForm.addEventListener('submit', event => {
-//     event.preventDefault(); /*the default behavior is for the page to reload, therefore we need to prevent Default */
-//
-//     // get the text
-//     let title = newToDoText.value; //assign variable "title"
-//
-//     // create a new li
-//     let newLi = document.createElement('li');
-//     // create a new input
-//     let checkbox = document.createElement('input');
-//     // set the input's type to checkbox
-//     checkbox.type = "checkbox";
-//
-//     //set the title
-//     newLi.textContent = title;
-//     //attach the checkbox to the li
-//     newLi.appendChild(checkbox);
-//     toDoList.appendChild(newLi);
-//
-//     //empty the input
-//     newToDoText.value = '';
-//
-//   });
-// /
-// deleteButtonFuction???
-//   deleteButton.addEventListener('click', function() {
-//     event.preventDefault();
-//
-//     for (i=toDoList.children.length-1; i>=0; i--) {
-//       if (toDoList.children[i].children[0]) {
-//         toDoList.removeChild(toDoList.children[i])
-//       }
-//
-//     }
-//
-//
-//   });
-// <button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" id="deleteButton">Delete</button>
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   let id = 0;
@@ -63,6 +20,8 @@ function onReady() {
     renderTheUI();
 
   }
+}
+
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
@@ -113,3 +72,48 @@ function onReady() {
 window.onload = function() {
   onReady();
 };
+
+
+// const addToDoForm = document.getElementById('addToDoForm')
+//   const newToDoText = document.getElementById('newToDoText')
+//   const toDoList = document.getElementById('toDoList')
+//   //const deleteButton = document.getElementById('deleteButton') //access the element deleteButton
+//
+//   addToDoForm.addEventListener('submit', event => {
+//     event.preventDefault(); /*the default behavior is for the page to reload, therefore we need to prevent Default */
+//
+//     // get the text
+//     let title = newToDoText.value; //assign variable "title"
+//
+//     // create a new li
+//     let newLi = document.createElement('li');
+//     // create a new input
+//     let checkbox = document.createElement('input');
+//     // set the input's type to checkbox
+//     checkbox.type = "checkbox";
+//
+//     //set the title
+//     newLi.textContent = title;
+//     //attach the checkbox to the li
+//     newLi.appendChild(checkbox);
+//     toDoList.appendChild(newLi);
+//
+//     //empty the input
+//     newToDoText.value = '';
+//
+//   });
+// /
+// deleteButtonFuction???
+//   deleteButton.addEventListener('click', function() {
+//     event.preventDefault();
+//
+//     for (i=toDoList.children.length-1; i>=0; i--) {
+//       if (toDoList.children[i].children[0]) {
+//         toDoList.removeChild(toDoList.children[i])
+//       }
+//
+//     }
+//
+//
+//   });
+// <button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" id="deleteButton">Delete</button>
