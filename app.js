b125f558d7cbbc4e3eadcb3592daed5b100cd0ef
dotmao <dorothy.hongqian@gmail.com>
2018-03-16 19:52:47 -0400

    updated files 03/16

--- app.js
+++ app.js
@@ -4,20 +4,43 @@ function onReady() {
   let id = 0;
   const submitButton = document.getElementById('submitButton');
 
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = ''; //set it as a empty value in the beginning before user input anything.
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');  //creating new li
+      const checkbox = document.createElement('input');  //assign the toDo's title text to the li
+      // const deleteButton = document.createElement('button');
+      checkbox.type = "checkbox";
+
+
+      newLi.textContent = toDo.title;
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      // newLi.appendChild(deleteButton);
+
+    });
+  }
+
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
+    const deleteButton = document.createElement('deleteButton'); //creating deletebutton in JS
+
+
+
 
 
     if (!newToDoText.value) { return; }
-//to add newToDos
+//to add newToDos avoid submitting empty value
     toDos.push ({
       title: newToDoText.value,
       complete: false,  //since it is a new 'todo', mark checkbox complete function needs to be off
       id: id++ // increment id variable
     });
     newToDoText.value = ''; //clear the text field for users
-
-    renderTheUI();
+  }
 
     function renderTheUI() {
       const toDoList = document.getElementById('toDoList');
@@ -28,6 +51,10 @@ function onReady() {
       toDos.forEach(function(toDo) {
         const newLi = document.createElement('li');  //creating new li
         const checkbox = document.createElement('input');  //assign the toDo's title text to the li
+        const deleteButton = document.createElement('input'); //input is linked to "delete" text
+        deleteButton.setAttribute("type", "button"); //setting the deleteButton attribute to "button" type.
+        deleteButton.setAttribute("value", "delete"); //setAttribute is a built-in method and always have two prep
+
         // const deleteButton = document.createElement('button');
         checkbox.type = "checkbox";
 
@@ -35,7 +62,25 @@ function onReady() {
         newLi.textContent = toDo.title;
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
+        newLi.appendChild(deleteButton);
         // newLi.appendChild(deleteButton);
+
+        deleteButton.addEventListener('click', event => {
+          event.preventDefault();
+
+
+          toDoList.removeChild(newLi)//deleting elements from html;
+
+          //todos.filter
+
+          const toRemoveId = toDo.id;
+          const filteredTodos = toDos.filter(toDo => toDo.id !== toRemoveId);
+          todos = filteredTodos;//the new array after deletion
+
+
+
+
+        });
 
       });
     }
@@ -46,26 +91,11 @@ function onReady() {
       newToDoText = '';
       renderTheUI();
     });
+    renderTheUI();
 
   }
-}
 
 
-
-//   deleteButton.addEventListener('click', event => {
-//     event.preventDefault();
-//
-//     const toRemoveId = 1;
-//     const filteredTodos = toDos.filter(toDo => toDo.id !== toRemoveId);
-//
-//
-//   });
-//
-//   submitButton.addEventListener('submit', event => {
-//     renderTheUI();
-//     });
-//
-// }
 
 
 
